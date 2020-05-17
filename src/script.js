6a0e9bfdd790a302d998a035bf50819d3287d074
akuto aoyama <aaaa.akuto@gmail.com>
2020-05-17 09:40:10 +0900

    index.html変更、css削除

--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
 
 window.onload = function () {
     getValue();
-    var $formObject = document.getElementById( "sampleForm" );
+    var $formObject = document.getElementById( "input-form" );
     for( var $i = 0; $i < $formObject.length; $i++ ) {
         $formObject.elements[$i].onkeyup = function(){
             getValue();
@@ -13,6 +13,6 @@ window.onload = function () {
     document.getElementById( "sampleOutputLength" ).innerHTML = $formObject.length;
 }
 function getValue() {
-    var $formObject = document.getElementById( "sampleForm" );
+    var $formObject = document.getElementById( "input-form" );
     document.getElementById( "sampleOutputName" ).innerHTML = $formObject.formName.value;
 }

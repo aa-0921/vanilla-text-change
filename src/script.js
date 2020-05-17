88825658bd12b7e5bd370a19208d5687e2d10fcc
akuto aoyama <aaaa.akuto@gmail.com>
2020-05-17 09:58:32 +0900

    index.html修正

--- src/script.js
+++ src/script.js
@@ -1,18 +1,4 @@
-
-window.onload = function () {
-    getValue();
-    var $formObject = document.getElementById( "input-form" );
-    for( var $i = 0; $i < $formObject.length; $i++ ) {
-        $formObject.elements[$i].onkeyup = function(){
-            getValue();
-        };
-        $formObject.elements[$i].onchange = function(){
-            getValue();
-        };
-    }
-    document.getElementById( "sampleOutputLength" ).innerHTML = $formObject.length;
-}
 function getValue() {
     var $formObject = document.getElementById( "input-form" );
-    document.getElementById( "sampleOutputName" ).innerHTML = $formObject.formName.value;
+    document.getElementById( "output" ).innerHTML = $formObject.inputForm.value;
 }
